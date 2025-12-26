52b486d8a20ea258f896bfb82e97ffcc0159a40f
benlamineyassine86-sys <benlamineyassine86@gmail.com>
2025-12-26 11:50:33 +0100

    ajout de sons

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import pytmx
 import pyscroll
 from src.player import Player
 
+
 def main():
     pygame.init()
+    pygame.mixer.init()
+    mmo_sound = pygame.mixer.Sound("assets/sounds/clear-combo-7-394494-_1_.wav")
+    step_sound = pygame.mixer.Sound("assets/sounds/walking-on-concrete-ver-2-268513.wav")
+    step_sound.set_volume(0.4)
+
+    walking = False
+
+    font = pygame.font.SysFont(None, 60)
+    show_mmo = False
+
     screen_width, screen_height = 800, 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Donjon test")
@@ -18,26 +29,53 @@ def main():
     group.add(player)
     running = True
     while running:
-        
+        moving = False
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_z] :
             player.move_up()
+            moving = True
         elif keys[pygame.K_s] :
             player.move_down()
+            moving = True
 
         if keys[pygame.K_q] :
             player.move_left()
+            moving = True
         elif keys[pygame.K_d] :
             player.move_right()
+            moving = True
+            
+        if moving and not walking:
+            step_sound.play(-1)  # boucle
+            walking = True
+
+        elif not moving and walking:
+            step_sound.stop()
+            walking = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_j:
+                    mmo_sound.play()
+                    show_mmo = True
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_j:
+                    show_mmo = False
+
+
         player.update()
         group.center(player.rect.center)   
         group.draw(screen)
+        
+        if show_mmo:
+            text = font.render("MENU", True, (255, 255, 255))
+            screen.blit(text, (20, 20))
+
         pygame.display.flip()
         clock.tick(60)
 
